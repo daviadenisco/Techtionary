912389164a4da8466ee862bcb5ae2313130bac50
Davia DeNisco <davia622@gmail.com>
2018-01-18 19:46:39 -0500

    Add db.js

--- server.js
+++ server.js
@@ -1,3 +1,30 @@
 const express = require('express');
 const app = express();
 const PORT = 8000;
+
+/*
+  GET ALL WORDS
+*/
+app.get('/birds', (req, res) => {});
+
+/*
+  CREATE A WORD
+*/
+app.post('/birds', (req, res) => {});
+
+/*
+  FETCH A WORD
+*/
+app.get('/birds/:bird_id', (req, res) => {});
+
+/*
+  PATCH A WORD
+*/
+app.patch('/birds/:bird_id', (req, res) => {});
+
+/*
+  DELETE A WORD
+*/
+app.delete('/birds/:bird_id', (req, res) => {});
+
+app.listen(PORT, () => console.log('Listening on', PORT))
